943a5b4f3bd49f0a848254a6b6598cb30c0dd3ff
Taylor Harvin <tharvin12@gmail.com>
2016-06-16 08:30:05 -0500

    test

--- js/MessageSender.js
+++
@@ -1,6 +0,0 @@
-function GetClassMessages(){
-	$.post("serverside/GrabMessage.php", {}, function(res){
-		// How to access HTML element with JQuery and set it's value
-		$("#allMsg").val(res);
-	});
-}
--- serverside/includes/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define('DB_HOST','localhost');						// The server address
-	define('DB_NAME','code_camp');					// The database name
-	define('DB_USER','code_camp');					// The database user name
-	define('DB_PASS','myPassword');	// The database password
-?>
